17b990a5d5ed1633e104953d01a3125ed7ac91ab
Ariel Fogel <fogeltine@gmail.com>
2014-05-20 18:08:58 -0400

    first 2 challenges done

--- week_5/1_die/my_solution.rb
+++ week_5/1_die/my_solution.rb
@@ -5,24 +5,40 @@
 
 # 2. Pseudocode
 
-# Input:
-# Output:
+# Input: an integer greater than 0
+# Output: N/A...creates an object and two methods for that type of object
 # Steps:
+
+# Define initialize method, taking a single integer parameter
+# If the parameter is less than 1
+# raise an Argument Error
+# then define the instance variable sides based the parameter
+# end function
+# define the sides method, with no parameters
+# return the instance variable sides
+# end method
+# define the roll method, with no parameters
+# return a random number between 1 and the number of sides
+# end method
 
 
 # 3. Initial Solution
 
 class Die
   def initialize(sides)
-    # code goes here
+  	unless sides>=1
+  		raise ArgumentError.new("Die must have at least one side")
+  	end
+
+  	@sides = sides
   end
   
   def sides
-    # code goes here
+    return @sides
   end
   
   def roll
-    # code goes here
+    return rand(1..@sides)
   end
 end
 
@@ -30,16 +46,53 @@ end
 
 # 4. Refactored Solution
 
-
+class Die
+  def initialize(sides)
+  	raise ArgumentError.new("Die must have at least one side") if sides<1
+  	@sides = sides
+  end
+  
+  def sides
+    return @sides
+  end
+  
+  def roll
+    return rand(1..@sides)
+  end
+end
 
 
 
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
 
-
+# puts Die.new(0)==ArgumentError
+die = Die.new(6)
+puts die.sides==6
+puts die.roll.between?(1, 6)
 
 
 
 
 # 5. Reflection 
+# What parts of your strategy worked? What problems did you face?
+# All parts of the strategy worked. No issues or problems to speak of.
+# 
+# What questions did you have while coding? What resources did you find to help you answer them?
+# Do I need to require any modules to call a method that randomizes? The answer was no.
+# This page was nice: http://ruby.about.com/od/newinruby191/ss/Generating-Random-Numbers-In-Ruby.htm
+# 
+# What concepts are you having trouble with, or did you just figure something out? If so, what?
+# None.
+# 
+# Did you learn any new skills or tricks?
+# Not really.
+# 
+# How confident are you with each of the Learning Competencies?
+# confident.
+# 
+# Which parts of the challenge did you enjoy?
+# This was a very quick challenge for me.
+# 
+# Which parts of the challenge did you find tedious?
+# None.
--- week_5/2_die/my_solution.rb
+++ week_5/2_die/my_solution.rb
@@ -5,21 +5,37 @@
 
 # 2. Pseudocode
 
-# Input:
-# Output:
+# Input: an array
+# Output: N/A...creates an object and two methods for that type of object
 # Steps:
+
+# Define initialize method, taking a single Array as a parameter
+# If the parameter has fewer than 1 element
+# raise an Argument Error
+# then define the instance variable sides based the parameter
+# end function
+# define the sides method, with no parameters
+# return the number of elements within the input Array
+# end method
+# define the roll method, with no parameters
+# return a random element from within the Array
+# end method
 
 
 # 3. Initial Solution
 
 class Die
   def initialize(labels)
+  	raise ArgumentError.new("Die must have at least one side") if labels.length<1
+  	@labels = labels
   end
 
   def sides
+  	return @labels.length
   end
 
   def roll
+  	return @labels.sample
   end
 end
 
@@ -27,16 +43,51 @@ end
 
 # 4. Refactored Solution
 
+class Die
+  def initialize(labels)
+  	raise ArgumentError.new("Die must have at least one side") if labels.length<1
+  	@labels = labels
+  end
 
+  def sides
+  	return @labels.length
+  end
+
+  def roll
+  	return @labels.sample
+  end
+end
 
 
 
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
 
-
+die = Die.new(['A', 'B', 'C', 'D', 'E', 'F'])
+puts die.sides==6
+puts die.roll.between?('A','F')
 
 
 
 
 # 5. Reflection 
+# What parts of your strategy worked? What problems did you face?
+# All parts of the strategy worked. No issues or problems to speak of.
+# 
+# What questions did you have while coding? What resources did you find to help you answer them?
+# Only syntax questions re: the Array#sample method. I used Ruby Docs as the resource.
+# 
+# What concepts are you having trouble with, or did you just figure something out? If so, what?
+# None.
+# 
+# Did you learn any new skills or tricks?
+# Not really.
+# 
+# How confident are you with each of the Learning Competencies?
+# confident.
+# 
+# Which parts of the challenge did you enjoy?
+# This was a very quick challenge for me.
+# 
+# Which parts of the challenge did you find tedious?
+# None.
